1a59b56e1a7e814c34a3ab2822052cd6f7aab5c2
Arslan Tariq <99298951+ArslanTariq575@users.noreply.github.com>
2024-10-11 23:13:26 -0400

    Fix About Page and made small changes to Home Page also Register background was config properly for phone screen. It all of the Issue

--- watchdog_script.py
+++ watchdog_script.py
@@ -25,21 +25,21 @@ class WatchdogHandler(FileSystemEventHandler):
 
     def on_any_event(self, event):
         """Restart Flask app when a file system change occurs."""
-        if event.src_path.endswith(".py") or event.src_path.endswith(".html"):
+        if event.src_path.endswith(".py") or event.src_path.endswith(".html") or event.src_path.endswith(".css"):
             print(f"Detected change in {event.src_path}. Restarting process...")
             self.stop_process()
             self.start_process()
 
 if __name__ == "__main__":
-    # Watch current directory and templates folder
-    paths_to_watch = [".", "./templates"]
+    # Watch current directory, templates folder, and static/css folder
+    paths_to_watch = [".", "./templates", "./static/css"]
 
     command = "flask run"  # Command to run Flask
 
     event_handler = WatchdogHandler(command)
     observer = Observer()
 
-    # Watch both the Python files and templates folder for changes
+    # Watch Python, HTML, and CSS files for changes
     for path in paths_to_watch:
         observer.schedule(event_handler, path, recursive=True)
 
